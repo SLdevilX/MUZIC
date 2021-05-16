7b29eae528ba1089f11f3e41e64c74bb2a640c74
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-17 00:06:52 +0530

    Update muzic.py

--- muzic.py
+++ muzic.py
@@ -18,43 +18,16 @@ bot = Client(
     api_hash = Config.API_HASH
 )
 
-## Extra Fns -------------------------------
-
-# Convert hh:mm:ss to seconds
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
 
 ## Commands --------------------------------
-@bot.on_message(filters.command(['start']))
-def start(client, message):
-    Sldevilx = f'╭─━━━━━━━━━━━━━━━━━─╮\n\n       🤟 Hey @{message.from_user.username} [😏🎧](https://telegra.ph/file/534ba62f07c64c5fb25ef.jpg)\n      Welcome to X-Troid MUZIC \n To use me Type with this format👇 \n           /song song name\n\n╰─━━━━━━━━━━━━━━━━━─╯'
-    message.reply_text(
-        text=Sldevilx, 
-        quote=False,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('Owner🎸', url='http://t.me/Danuma_admin_bot'),
-                    InlineKeyboardButton('About Owner💡', url='https://lasiya.ml'),
-                ],
-                [
-                    InlineKeyboardButton('Main Group 🇱🇰', url='http://t.me/Danuma01')
-                ],
-                [
-                    InlineKeyboardButton('Bot Channel 🏅', url='http://t.me/danumabots')
-                 ]
-            ]
-            
-        )
-    )
+
     
  @bot.on_message(filters.command(['help']))
 def help(client, message):
     Lasi = f'Respect to Dev & Give credits When you edit it,\n Dont foget to Give STAR to Original Repo.'
     message.reply_text(
-        text=Sldevilx, 
+        text= Lasi, 
         quote=False,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -66,70 +39,6 @@ def help(client, message):
         )
     )
 
-@bot.on_message(filters.command(['song']))
-def song(client, message):
-    query = ''
-    for i in message.command[1:]:
-        query += ' ' + str(i)
-    print(query)
-    m = message.reply('🔎 Searching the song...')
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = []
-        count = 0
-        while len(results) == 0 and count < 6:
-            if count>0:
-                time.sleep(1)
-            results = YoutubeSearch(query, max_results=1).to_dict()
-            count += 1
-        # results = YoutubeSearch(query, max_results=1).to_dict()
-        try:
-            link = f"https://youtube.com{results[0]['url_suffix']}"
-            # print(results)
-            title = results[0]["title"]
-            thumbnail = results[0]["thumbnails"][0]
-            duration = results[0]["duration"]
 
-            ## UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            # if time_to_seconds(duration) >= 1800:  # duration limit
-            #     m.edit("Exceeded 30mins cap")
-            #     return
-
-            views = results[0]["views"]
-            thumb_name = f'thumb{message.message_id}.jpg'
-            thumb = requests.get(thumbnail, allow_redirects=True)
-            open(thumb_name, 'wb').write(thumb.content)
-
-        except Exception as e:
-            print(e)
-            m.edit('Found nothing. Try changing the spelling a little.')
-            return
-    except Exception as e:
-        m.edit(
-            "✖️ Found Nothing. Sorry.\n\nTry another keywork or maybe spell it properly."
-        )
-        print(str(e))
-        return
-    m.edit("⏬ Downloading.")
-    try:
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f'🎧 **Title**: [{title[:35]}]({link})\n⏳ **Duration**: `{duration}`\n👁‍🗨 **Views**: `{views}`'
-        secmul, dur, dur_arr = 1, 0, duration.split(':')
-        for i in range(len(dur_arr)-1, -1, -1):
-            dur += (int(dur_arr[i]) * secmul)
-            secmul *= 60
-        message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name)
-        m.delete()
-    except Exception as e:
-        m.edit('❌ Error')
-        print(e)
-    try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
-    except Exception as e:
-        print(e)
 
 bot.run()
