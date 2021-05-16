91a1dbdf7be84a296e825a6579e5a64874405bcf
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-05-16 23:19:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     BOT_TOKEN = "1629564290:AAH5RVRgDMLMVMVjSFgWo8bcLP0iL7x5Poc" # 🌚 get it from @botfather (telegram) by sending /newbot command.
     API_ID = "3516020" # 🌚 Same as APP_ID. Get it from my.telegram.org.
-    API_HASH = "1629564290:AAGMHB0KyM_HKfWw7-5QhOSCgHjOzl42Z5k" # 🌚 Get it from my.telegram.org.
+    API_HASH = "1822638971:AAFF3oskUXg7SBqk4Te_k_yeL6WVZqEmmh8" # 🌚 Get it from my.telegram.org.
