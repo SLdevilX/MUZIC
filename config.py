86139206123fde2e602d7ccd6ec6c089f331408c
Lasiya <52373359+SLdevilX@users.noreply.github.com>
2021-08-02 15:53:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")  # from @botfather
     API_ID = int(os.getenv("API_ID"))  # from https://my.telegram.org/apps
